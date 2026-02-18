59669827e9fd48b5f6e023e512284ce2394b3212
Your Name <jojodude569@gmail.com>
2026-02-18 14:35:22 -0700

    Phase 3: Clear and Create Game Handlers

--- server/src/main/java/handler/ClearHandler.java
+++ server/src/main/java/handler/ClearHandler.java
@@ -1,4 +1,19 @@
 package handler;
 
+import io.javalin.http.Context;
+
 public class ClearHandler {
+
+    private final dataaccess.DataAccess dataAccess;
+
+    public ClearHandler(dataaccess.DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public void handle(Context ctx) {
+        //FIXME: clear() not working
+        //dataAccess.clear();
+        ctx.status(200);
+        ctx.result("{}");
+    }
 }
--- server/src/main/java/handler/CreateGameHandler.java
+++ server/src/main/java/handler/CreateGameHandler.java
@@ -1,4 +1,42 @@
 package handler;
 
+import com.google.gson.Gson;
+import io.javalin.http.Context;
+import request.CreateGameRequest;
+import result.CreateGameResult;
+import result.ErrorResult;
+import service.GameService;
+
 public class CreateGameHandler {
+
+    private final GameService gameService;
+    private final Gson gson = new Gson();
+
+    public CreateGameHandler(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    public void handle(Context ctx) {
+        try {
+            String authToken = ctx.header("Authorization");
+
+            CreateGameRequest request =
+                    gson.fromJson(ctx.body(), CreateGameRequest.class);
+
+            int gameID =
+                    gameService.createGame(authToken, request.gameName());
+
+            ctx.status(200);
+            ctx.json(new CreateGameResult(gameID));
+
+        } catch (Exception e) {
+            setStatus(ctx, e.getMessage());
+            ctx.json(new ErrorResult(e.getMessage()));
+        }
+    }
+
+    private void setStatus(Context ctx, String msg) {
+        if (msg.contains("unauthorized")) ctx.status(401);
+        else ctx.status(400);
+    }
 }
